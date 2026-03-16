d47fb4c8aaa4459aae41bd5cb26e889167db89ee
vishallende24 <vishallende24@gmail.com>
2026-03-16 16:36:29 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
-  base: "/",
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler"]],
+            },
+        }),
+    ],
+    base: "/Portfolio/",
 
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-  },
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        sourcemap: false,
+    },
 
-  server: {
-    port: 5173,
-  },
+    server: {
+        port: 5173,
+    },
 });
